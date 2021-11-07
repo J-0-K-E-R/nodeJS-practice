e7ffea7eab29eb1ddcfd0d76b5053eaecd074876
J-0-k-e-r <aman1334@yahoo.com>
2021-11-07 14:56:47 +0530

    Added os module

---
+++ Tests/09. os module/os.js
@@ -0,0 +1,25 @@
+const os = require('os')
+
+// Platform
+console.log(os.platform())
+
+
+// CPU Arch
+console.log(os.arch())
+
+
+// CPU Core Info
+console.log(os.cpus())
+
+
+// Memory
+console.log(os.freemem())
+console.log(os.totalmem())
+
+
+// Home dir
+console.log(os.homedir())
+
+
+// Uptime
+console.log(os.uptime())
